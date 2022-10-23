b411e4cfc303149dbc0a6d69b228456d05f68b97
saro2808 <saro.harutyunyan@gmail.com>
2022-10-23 14:51:12 +0300

    passed all sorted tests

--- task2/FixedString.hpp
+++ task2/FixedString.hpp
@@ -1,12 +1,33 @@
 #pragma once
 
+#include <string_view>
 
 template<size_t max_length>
 struct FixedString {
-  FixedString(const char* string, size_t length);
-  operator std::string_view() const;
-
-  // std::string impl; ???
+	constexpr FixedString() = default;
+	constexpr FixedString(const FixedString&) = default;
+	constexpr FixedString(FixedString&&) = default;
+	
+	constexpr FixedString(const char* string, size_t length)
+		: len(max_length >= length ? length : max_length) {
+		for (int i = 0; i < len; ++i) {
+			str[i] = string[i];
+		}
+	}
+	
+	constexpr ~FixedString() = default;
+	
+	constexpr FixedString& operator=(const FixedString&) = default;
+	constexpr FixedString& operator=(FixedString&&) = default;
+	
+	constexpr operator std::string_view() const {
+		return std::string_view(str, len);
+	}
+	
+	char str[max_length];
+	size_t len;
 };
 
-// operator ""_cstr ?
+constexpr FixedString<256> operator""_cstr(const char* str, size_t len) {
+	return FixedString<256>(str, len);
+}
