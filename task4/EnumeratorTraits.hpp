ef983ac53a42d8479f1d33ff119882255f5d7138
saro2808 <saro.harutyunyan@gmail.com>
2022-11-25 18:51:06 +0300

    at() works too

--- task4/EnumeratorTraits.hpp
+++ task4/EnumeratorTraits.hpp
@@ -21,13 +21,31 @@ struct EnumeratorTraits {
         return isValid_<(Enum)0>() + positives.first + positives.second
                                    + negatives.first + negatives.second;
     }
-    /*
-    static constexpr Enum at(std::size_t i) noexcept {
-        constexpr auto negativesSize = signedSize_<-1, low_()>();
-        constexpr auto positivesSize = signedSize_<1, high_()>();
+    
+    static constexpr Enum at(const std::size_t i) noexcept {
+        constexpr auto negativesSizes = signedSize_<-1, low_()>();
+        constexpr auto positivesSizes = signedSize_<1, high_()>();
+        constexpr auto negativesSize = negativesSizes.first + negativesSizes.second;
+        constexpr auto positivesSize = positivesSizes.first + positivesSizes.second;
         constexpr auto zeroIsValid = isValid_<(Enum)0>();
-        if constexpr (i < negativesSize)
-    }*/
+        
+        if (i < negativesSize) {
+            // loop over negatives
+            if (i < negativesSizes.second)
+                return getValueAt_<-1, low_()/2, low_() - low_()/2>(i);
+            else
+                return getValueAt_<-1, 0, low_()/2>(i - (int)negativesSizes.second);
+        } else {
+            if constexpr (zeroIsValid)
+                if (i == negativesSize)
+                    return (Enum)0;
+            // loop over positives
+            if (i < negativesSize + zeroIsValid + positivesSizes.first)
+                return getValueAt_<1, 0, high_()/2>(i - (int)zeroIsValid - (int)negativesSize);
+            else
+                return getValueAt_<1, high_()/2, high_() - high_()/2>(i - (int)zeroIsValid - (int)negativesSize - (int)positivesSizes.first);
+        }
+    }
 
 //    static constexpr std::string_view nameAt(std::size_t i) noexcept;
 
@@ -43,8 +61,8 @@ private:
     template<signed char sign, std::size_t max>
     static constexpr auto signedSize_() noexcept {
         constexpr auto maxHalved = max / 2;
-        constexpr auto sz0 = /*(maxHalved == 0) ? 0 :*/ size_<sign,             1>(std::make_index_sequence<      maxHalved>{});
-        constexpr auto sz1 = /*(max       == 0) ? 0 :*/ size_<sign, maxHalved + 1>(std::make_index_sequence<max - maxHalved>{});
+        constexpr auto sz0 = size_<sign,             1>(std::make_index_sequence<      maxHalved>{});
+        constexpr auto sz1 = size_<sign, maxHalved + 1>(std::make_index_sequence<max - maxHalved>{});
         return std::make_pair(sz0, sz1);
     }
     
@@ -65,5 +83,21 @@ private:
             return 0;
         }
         return 1;
+    }
+
+    template<char sign, int offset, std::size_t max>
+    static constexpr Enum getValueAt_(const std::size_t i) {
+        int res = 0;
+        int counter = 0;
+        [&]<std::size_t... I>(std::index_sequence<I...>) {
+            (void)((res = [&]() {
+                if constexpr (isValid_<(Enum)(sign*(static_cast<int>(I)+1+offset))>())
+                    ++counter;
+                if (i + 1 == counter)
+                    return sign*(static_cast<int>(I)+1+offset);
+                return 0;
+            }(), i + 1 != counter) && ...);
+        }(std::make_index_sequence<max>{});
+        return (Enum)res;
     }
 };
